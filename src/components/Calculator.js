605af0a535393cb1fb1bb9a9368a9998f351e277
John Adibe <johnadibe123@gmail.com>
2023-02-16 12:55:06 -0800

    Add calculate.js and operate.js files

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,94 @@
-function Calculator() {
-  return (
-    <div className="container">
-      <form>
-        <input className="output" type="text" />
-      </form>
+import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
-      <div className="btns">
-        <button type="button" className="grey">AC</button>
-        <button type="button" className="grey">+/-</button>
-        <button type="button" className="grey">%</button>
-        <button type="button" className="orange">&divide;</button>
-        <button type="button" className="grey">7</button>
-        <button type="button" className="grey">8</button>
-        <button type="button" className="grey">9</button>
-        <button type="button" className="orange">&times;</button>
-        <button type="button" className="grey">4</button>
-        <button type="button" className="grey">5</button>
-        <button type="button" className="grey">6</button>
-        <button type="button" className="orange">&minus;</button>
-        <button type="button" className="grey">1</button>
-        <button type="button" className="grey">2</button>
-        <button type="button" className="grey">3</button>
-        <button type="button" className="orange">+</button>
-        <button type="button" className="grey span-2">0</button>
-        <button type="button" className="grey">.</button>
-        <button type="button" className="orange">=</button>
-      </div>
-    </div>
-  );
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+      next: null,
+      operator: null,
+    };
+  }
+
+    handleClick = (event) => {
+      const result = (calculate(this.state, event.target.textContent));
+      this.setState(result);
+    }
+
+    resultDisplay = (event) => {
+      this.setState({ total: event.target.textContent });
+    }
+
+    render() {
+      const { total, next, operation } = this.state;
+      return (
+        <div className="container">
+          <div className="output" onChange={this.resultDisplay}>
+            {total}
+            {operation}
+            {next}
+          </div>
+
+          <div className="btns">
+            <button type="button" className="grey" onClick={this.handleClick}>AC</button>
+            <button type="button" className="grey" onClick={this.handleClick}>+/-</button>
+            <button type="button" className="grey" onClick={this.handleClick}>%</button>
+            <button type="button" className="orange" onClick={this.handleClick}>÷</button>
+            <button type="button" className="grey" onClick={this.handleClick}>7</button>
+            <button type="button" className="grey" onClick={this.handleClick}>8</button>
+            <button type="button" className="grey" onClick={this.handleClick}>9</button>
+            <button type="button" className="orange" onClick={this.handleClick}>x</button>
+            <button type="button" className="grey" onClick={this.handleClick}>4</button>
+            <button type="button" className="grey" onClick={this.handleClick}>5</button>
+            <button type="button" className="grey" onClick={this.handleClick}>6</button>
+            <button type="button" className="orange" onClick={this.handleClick}>-</button>
+            <button type="button" className="grey" onClick={this.handleClick}>1</button>
+            <button type="button" className="grey" onClick={this.handleClick}>2</button>
+            <button type="button" className="grey" onClick={this.handleClick}>3</button>
+            <button type="button" className="orange" onClick={this.handleClick}>+</button>
+            <button type="button" className="grey span-2" onClick={this.handleClick}>0</button>
+            <button type="button" className="grey" onClick={this.handleClick}>.</button>
+            <button type="button" className="orange" onClick={this.handleClick}>=</button>
+          </div>
+        </div>
+      );
+    }
 }
 
 export default Calculator;
+
+// function Calculator() {
+//   return (
+//     <div className="container">
+//       <form>
+//         <input className="output" type="text" />
+//       </form>
+
+//       <div className="btns">
+//         <button type="button" className="grey">AC</button>
+//         <button type="button" className="grey">+/-</button>
+//         <button type="button" className="grey">%</button>
+//         <button type="button" className="orange">&divide;</button>
+//         <button type="button" className="grey">7</button>
+//         <button type="button" className="grey">8</button>
+//         <button type="button" className="grey">9</button>
+//         <button type="button" className="orange">&times;</button>
+//         <button type="button" className="grey">4</button>
+//         <button type="button" className="grey">5</button>
+//         <button type="button" className="grey">6</button>
+//         <button type="button" className="orange">&minus;</button>
+//         <button type="button" className="grey">1</button>
+//         <button type="button" className="grey">2</button>
+//         <button type="button" className="grey">3</button>
+//         <button type="button" className="orange">+</button>
+//         <button type="button" className="grey span-2">0</button>
+//         <button type="button" className="grey">.</button>
+//         <button type="button" className="orange">=</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Calculator;
